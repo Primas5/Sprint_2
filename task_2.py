388fcf75fa83bbd6048e3e4b3ca126cf8dc47e1b
Дорошин Алексей Евгеньевич <doroshin.aleksey@corp.inn-tech.su>
2025-10-28 10:46:15 +0300

    Added 1 corrected tasks

--- task_2.py
+++ task_2.py
@@ -8,16 +8,15 @@ class Movies:
 class Comedy(Movies):        
     def add_movie(self, movie):
         super().add_movie(movie)
-        return f'Комедии: {movie}'
+        return f'Комедии: {self.movies}'
 
 class Drama(Movies):
     def add_movie(self, movie):
         super().add_movie(movie)
-        return f'Драма: {movie}'
+        return f'Драма: {self.movies}'
     
 comedy = Comedy()
 drama = Drama()
 
 print(comedy.add_movie('Большой куш'))
 print(drama.add_movie('Оружейный барон'))
-
